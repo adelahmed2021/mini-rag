f06a941a3f7db9c4a9d13cac4f3754bdab2f1310
adel <adelahmed1232100@gimal.com>
2025-10-20 21:18:17 +0300

    process file

--- src/routes/data.py
+++ src/routes/data.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI,APIRouter,Depends,UploadFile,status
 from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings,Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController,ProcessController
 import aiofiles
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
+
 
 loger = logging.getLogger('uvicorn.error')
 
@@ -31,7 +33,7 @@ async def upload_data(project_id: str, file: UploadFile,
         
         project_dir_path = ProjectController().get_project_path(project_id=project_id)
        
-        file_path = data_controller.generate_uniqe_filename(
+        file_path,file_id = data_controller.generate_uniqe_filepath(
             orig_file_name=file.filename,
             project_id=project_id
         )
@@ -52,6 +54,37 @@ async def upload_data(project_id: str, file: UploadFile,
                 )
         return JSONResponse(
                 content={
-                    'signal':ResponseSignal.FILE_UPLOAD_SUCCESS.value
+                    'signal':ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                    'file_id':file_id
                 }
             )
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str, process_request:ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+
+    process_controller  = ProcessController(project_id=project_id)
+    file_content = process_controller.get_file_content(file_id=file_id)
+    
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                'signal': ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+    return file_chunks
+
+
+
